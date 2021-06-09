b6294e1e70b79e5346433d3dbfee1f7ce226bc8f
GuuTAR <guitar20122012@gmail.com>
2021-06-09 20:25:34 +0700

    Add check icon at result

--- src/component/ForgetFormFinish/ForgetFormFinish.js
+++ src/component/ForgetFormFinish/ForgetFormFinish.js
@@ -1,3 +1,5 @@
+import { faCheckCircle } from '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useHistory } from 'react-router-dom'
 import './styles.scss'
 
@@ -12,10 +14,13 @@ const ForgetFormFinish = () => {
     return (
         <div className="forget-form-finish-comp flex-col ali-cent form">
             <h2 className="form-title margin-zero">ลืมรหัสผ่าน</h2>
-            <p className="margin-zero">
-                ระบบได้ส่ง mail ไป email ดังกล่าวแล้ว<br />
-                โปรดตรวจสอบที่ email ข้างต้น
-            </p>
+            <div className="result-content flex-row">
+                <FontAwesomeIcon className="check-icon" icon={faCheckCircle} />
+                <p className="margin-zero">
+                    ระบบได้ส่ง mail ไป email ดังกล่าวแล้ว<br />
+                    โปรดตรวจสอบที่ email ข้างต้น
+                </p>
+            </div>
             <button className="blue-sky-btn" onClick={handleClick}>ย้อนกลับ</button>
         </div>
     )
