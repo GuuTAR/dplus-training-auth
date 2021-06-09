6e679807ac33b5f66dbe042bb10bd011092629da
GuuTAR <guitar20122012@gmail.com>
2021-06-09 20:01:08 +0700

    Add Register Page

---
+++ src/page/Register/Register.js
@@ -0,0 +1,11 @@
+import RegisterForm from '../../component/RegisterForm/RegisterForm'
+import './styles.scss'
+const Register = () => {
+    return (
+        <div className="register-page cent-page full-h">
+            <RegisterForm />
+        </div>
+    )
+}
+
+export default Register
