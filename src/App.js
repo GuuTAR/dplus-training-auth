1da8ec20596b479d268c7f3a08a629f5cec7207c
GuuTAR <guitar20122012@gmail.com>
2021-06-09 19:58:27 +0700

    Change switch to RouterSlider Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  useHistory
-} from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
 
 import './App.scss'
-
-import Login from "./page/Login/Login";
-import Register from "./page/Register/Register";
+import RouterSlider from "./component/RouterSlider/RouterSlider";
 
 const App = () => {
-
-  const history = useHistory()
-
   return (
     <div className="App" >
-      <Router history={history}>
-        <Switch>
-          <Route exact key="login" path="/" component={Login} />
-          <Route exact key="register" path="/register" component={Register} />
-        </Switch>
+      <Router>
+        <RouterSlider />
       </Router>
     </div>
   )
