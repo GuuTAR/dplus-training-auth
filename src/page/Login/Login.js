3050110cb31bca26a55fbb7346cbf4cfbb8e24aa
GuuTAR <guitar20122012@gmail.com>
2021-06-09 16:55:26 +0700

    Add Login Page

---
+++ src/page/Login/Login.js
@@ -0,0 +1,13 @@
+import './styles.scss'
+
+import LoginForm from '../../component/LoginForm/LoginForm'
+
+const Login = () => {
+    return (
+        <div className="login-page cent-page full-h">
+            <LoginForm />
+        </div>
+    )
+}
+
+export default Login
