06efd3211b2ef0ca63d985171484e51d2f5c9f97
GuuTAR <guitar20122012@gmail.com>
2021-06-09 17:02:27 +0700

    Add route to Register Page

--- src/App.js
+++ src/App.js
@@ -5,18 +5,24 @@ import {
   useHistory
 } from "react-router-dom";
 
+import './App.scss'
+
 import Login from "./page/Login/Login";
+import Register from "./page/Register/Register";
 
 const App = () => {
 
   const history = useHistory()
 
   return (
-    <Router history={history}>
-      <Switch>
-        <Route exact key="login" path="/" component={Login} />
-      </Switch>
-    </Router>
+    <div className="App" >
+      <Router history={history}>
+        <Switch>
+          <Route exact key="login" path="/" component={Login} />
+          <Route exact key="register" path="/register" component={Register} />
+        </Switch>
+      </Router>
+    </div>
   )
 }
 
