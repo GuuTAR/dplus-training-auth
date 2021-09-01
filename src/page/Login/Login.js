c8c475180a67fca05bf6d9416682fb28819198a3
GuuTAR <guitar20122012@gmail.com>
2021-09-01 16:35:50 +0700

    Test API

--- src/page/Login/Login.js
+++ src/page/Login/Login.js
@@ -1,8 +1,17 @@
 import './styles.scss'
 
+import { useEffect } from 'react'
+
 import LoginForm from '../../component/LoginForm/LoginForm'
 
 const Login = () => {
+
+    useEffect(() => {
+        fetch("http://localhost:8000/api/user/users")
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [])
+
     return (
         <div className="login-page cent-page full-h">
             <LoginForm />
